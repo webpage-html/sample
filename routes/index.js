73bcc1c730de1ec75b2ec96d5381a3a67be198b0
uvaralakshmi <luckyummella@gmail.com>
2019-05-29 16:03:20 +0530

    sample

--- routes/index.js
+++ routes/index.js
@@ -11,17 +11,20 @@ router.post('/signup',function(req,res){
 	var a=req.body.name;
 	console.log(a);
 
-	var c=req.body.email;
-	console.log(c);
-
-	var d=req.body.bod
-	console.log(d);
-
-	var b=req.body.password;
+	var b=req.body.email;
 	console.log(b);
 
-	var e=req.body.number;
+	var c=req.body.gender;
+	console.log(c);
+
+	var e=req.body.dob
 	console.log(e);
+
+	var f=req.body.password;
+	console.log(f);
+
+	var g=req.body.number;
+	console.log(g);
 	res.redirect("/")
 });
 
