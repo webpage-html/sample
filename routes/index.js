cb067b8e9c2eac8584f3280b1f9bc903f6a6a1f5
uvaralakshmi <luckyummella@gmail.com>
2019-05-30 11:16:21 +0530

    monk

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var monk=require('monk')
+var db=monk('localhost:27017/aditya');
+console.log('connected')
+var collection=db.get('signup')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,21 +14,22 @@ router.get('/', function(req, res, next) {
 router.post('/signup',function(req,res){
 	var a=req.body.name;
 	console.log(a);
-
 	var b=req.body.email;
 	console.log(b);
-
 	var c=req.body.gender;
 	console.log(c);
-
-	var e=req.body.dob
+	var d=req.body.languages;
+	console.log(d);
+	var e=req.body.select;
 	console.log(e);
-
-	var f=req.body.password;
+	var f=req.body.dob
 	console.log(f);
-
-	var g=req.body.number;
+	var g=req.body.pswd;
 	console.log(g);
+	var h=req.body.number;
+	console.log(h);
+	// console.log(req.body)
+	collection.insert({"name":a,"email":b,"gender":c,"languages":d,"EBC":e,"dob":f,"password":g,"mobile no":h})
 	res.redirect("/")
 });
 
